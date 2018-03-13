9ad81c980cfc31decf44ad11300c93d10ee8c135
Taufeq Razakh <33366769+TaufeqRazakh@users.noreply.github.com>
2018-03-13 12:45:37 -0700

    Add files via upload

---
+++ KarateChop.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KarateChop : MonoBehaviour {
+
+	public SteamVR_TrackedObject hand;
+	private Rigidbody rBody;
+
+	// Use this for initialization
+	void Start () {
+		rBody = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rBody.MovePosition (hand.transform.position);
+		rBody.MoveRotation (hand.transform.rotation);
+	}
+}
